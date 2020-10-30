e63a7815e0eb34aa8a4fd7deb4d0cf6a92808d96
naruthsithasom <ett0088@gmail.com>
2020-10-30 21:42:57 +0700

    Table antdesign

--- src/AntDesgin.js
+++ src/AntDesgin.js
@@ -1,10 +1,68 @@
 import React from 'react'
-import {table} from 'antd'
+import { Table } from 'antd'
+const student = [{
+  "first_name": "Nona",
+  "last_name": "St. Hill",
+  "email": "nsthill0@miitbeian.gov.cn",
+  "gender": "Female",
+  "university": "Istanbul Arel University"
+}, {
+  "first_name": "Felice",
+  "last_name": "Wilcocke",
+  "email": "fwilcocke1@nytimes.com",
+  "gender": "Male",
+  "university": "University of Maryland Baltimore County"
+}, {
+  "first_name": "Chester",
+  "last_name": "Ruske",
+  "email": "cruske2@spotify.com",
+  "gender": "Male",
+  "university": "Rafsanjan University of Medical Sciences"
+}, {
+  "first_name": "Christye",
+  "last_name": "Biddy",
+  "email": "cbiddy3@sakura.ne.jp",
+  "gender": "Female",
+  "university": "Universidad Cientifica Latinoamericana de Hidalgo"
+}, {
+  "first_name": "Naoma",
+  "last_name": "Bithell",
+  "email": "nbithell4@is.gd",
+  "gender": "Female",
+  "university": "Kashan University of Medical Sciences"
+}, {
+  "first_name": "Adore",
+  "last_name": "Spaldin",
+  "email": "aspaldin5@wunderground.com",
+  "gender": "Female",
+  "university": "Canisius College"
+}]
 
-function AppDesign(){
-  return(
+const columns = [
+  {
+    title: "ชื่อจริง",
+    dataIndex:'email'
+  },
+  {
+    title: 'เพศ',
+    dataIndex: 'gender'  
+  },
+  {
+    title: 'นามสกุล',
+    dataIndex: 'last_name'  
+  },
+  {
+    title:'มหาวิทยาลัย',
+    dataIndex:'university'
+  }
+]
+function AppDesign() {
+  return (
     <div>
       <h1>Test Ant AppDesign</h1>
+      <div>
+        <Table columns={columns} dataSource={student} />
+      </div>
     </div>
   )
 }
